8ee85646d2d5da5418968fb4ff86d1835a018191
Carolin Weitkamp <c.weitkamp@posteo.de>
2022-04-05 11:49:50 +0200

    delete quell

--- pages/api/carbon.js
+++ pages/api/carbon.js
@@ -1,5 +1,3 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
 export default async function handler(request, response) {
   try {
     const url = request.query.url;
