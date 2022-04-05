68e068a2ce52d9894a1e50efa3a45a82602c4c67
Carolin Weitkamp <c.weitkamp@posteo.de>
2022-04-05 16:23:30 +0200

    change the fetch

--- pages/api/greenhosts.js
+++ pages/api/greenhosts.js
@@ -15,31 +15,3 @@ export default async function handler(request, response) {
     response.status(500).json({ error: error.message });
   }
 }
-
-// try {
-//   const url  = request.query.url;
-
-//   const response = await fetch(
-//     `https://admin.thegreenwebfoundation.org/data/directory/`
-//   );
-//   const newData = JSON.parse(await response.text()).replace();
-//   // const data = JSON.parse(
-//   //   (await response.text()).replace(/^callback\((.*)\);/, '$1')
-//   // );
-//   console.log(newData);
-// } catch (error) {
-//   response.status(500).json({ error: error.message });
-// }
-// }
-
-//   if (fetch.ok && data) {
-//     response.status(200).json(data);
-//   } else {
-//     response.status(500);
-//   }
-// } catch (error) {
-//   response.status(500).json({ error: error.message });
-// }
-
-// const response = await fetch('bla')
-// const data = JSON.parse((await response.text()).replace(/^callback\((.*)\);/, '$1'))
