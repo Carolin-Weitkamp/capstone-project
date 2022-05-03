0b3e9119db703cc978999c11057bb900de382934
Carolin Weitkamp <c.weitkamp@posteo.de>
2022-05-03 13:45:09 +0200

    change auskommentiert

--- components/Menu/MenuItem.js
+++ components/Menu/MenuItem.js
@@ -1,57 +1,57 @@
-import * as React from 'react';
-import { motion } from 'framer-motion';
-import styled from 'styled-components';
-import Link from 'next/link';
+// import * as React from 'react';
+// import { motion } from 'framer-motion';
+// import styled from 'styled-components';
+// import Link from 'next/link';
 
-const variants = {
-  open: {
-    y: 0,
-    opacity: 1,
-    transition: {
-      y: { stiffness: 1000, velocity: -100 },
-    },
-  },
-  closed: {
-    y: 50,
-    opacity: 0,
-    transition: {
-      y: { stiffness: 1000 },
-    },
-  },
-};
+// const variants = {
+//   open: {
+//     y: 0,
+//     opacity: 1,
+//     transition: {
+//       y: { stiffness: 1000, velocity: -100 },
+//     },
+//   },
+//   closed: {
+//     y: 50,
+//     opacity: 0,
+//     transition: {
+//       y: { stiffness: 1000 },
+//     },
+//   },
+// };
 
-const colors = ['yellow', 'yellow', 'yellow', 'yellow'];
+// const colors = ['yellow', 'yellow', 'yellow', 'yellow'];
 
-export const MenuItem = ({ id, name, linkUrl }) => {
-  const style = { border: `2px solid ${colors[id]}` };
-  return (
-    <StyledLi
-      variants={variants}
-      whileHover={{ scale: 1.1 }}
-      whileTap={{ scale: 0.95 }}
-    >
-      <StyledTextPlaceholder style={style}>
-        <Link href={linkUrl} passHref>
-          {name}
-        </Link>
-      </StyledTextPlaceholder>
-    </StyledLi>
-  );
-};
+// export const MenuItem = ({ id, name, linkUrl }) => {
+//   const style = { border: `2px solid ${colors[id]}` };
+//   return (
+//     <StyledLi
+//       variants={variants}
+//       whileHover={{ scale: 1.1 }}
+//       whileTap={{ scale: 0.95 }}
+//     >
+//       <StyledTextPlaceholder style={style}>
+//         <Link href={linkUrl} passHref>
+//           {name}
+//         </Link>
+//       </StyledTextPlaceholder>
+//     </StyledLi>
+//   );
+// };
 
-const StyledTextPlaceholder = styled(motion.div)`
-  border-radius: 5px;
-  width: 200px;
-  height: 20px;
-  flex: 1;
-`;
+// const StyledTextPlaceholder = styled(motion.div)`
+//   border-radius: 5px;
+//   width: 200px;
+//   height: 20px;
+//   flex: 1;
+// `;
 
-const StyledLi = styled(motion.div)`
-  margin: 0;
-  padding: 0;
-  list-style: none;
-  margin-bottom: 20px;
-  display: flex;
-  align-items: center;
-  cursor: pointer;
-`;
+// const StyledLi = styled(motion.div)`
+//   margin: 0;
+//   padding: 0;
+//   list-style: none;
+//   margin-bottom: 20px;
+//   display: flex;
+//   align-items: center;
+//   cursor: pointer;
+// `;
--- components/Menu/Navigation.js
+++ components/Menu/Navigation.js
@@ -1,33 +1,33 @@
-import * as React from 'react';
-import { motion } from 'framer-motion';
-import { MenuItem } from './MenuItem';
-import styled from 'styled-components';
+// import * as React from 'react';
+// import { motion } from 'framer-motion';
+// import { MenuItem } from './MenuItem';
+// import styled from 'styled-components';
 
-const variants = {
-  open: {
-    transition: { staggerChildren: 0.07, delayChildren: 0.2 },
-  },
-  closed: {
-    transition: { staggerChildren: 0.05, staggerDirection: -1 },
-  },
-};
+// const variants = {
+//   open: {
+//     transition: { staggerChildren: 0.07, delayChildren: 0.2 },
+//   },
+//   closed: {
+//     transition: { staggerChildren: 0.05, staggerDirection: -1 },
+//   },
+// };
 
-export const Navigation = () => (
-  <StyledUl variants={variants}>
-    <MenuItem id={0} name={'Start'} linkUrl={'/'} />
-    <MenuItem id={1} name={'Rechner'} linkUrl={'/calculator'} />
-    <MenuItem id={2} name={'Grünes Hosting'} linkUrl={'/green-hosts'} />
-  </StyledUl>
-);
-// on Click bei menuItem
-// const itemIds = [0, 1, 2, 3];
+// export const Navigation = () => (
+//   <StyledUl variants={variants}>
+//     <MenuItem id={0} name={'Start'} linkUrl={'/'} />
+//     <MenuItem id={1} name={'Rechner'} linkUrl={'/calculator'} />
+//     <MenuItem id={2} name={'Grünes Hosting'} linkUrl={'/green-hosts'} />
+//   </StyledUl>
+// );
+// // on Click bei menuItem
+// // const itemIds = [0, 1, 2, 3];
 
-const StyledUl = styled(motion.ul)`
-  color: black;
-  margin: 0;
-  padding: 0;
-  padding: 25px;
-  position: absolute;
-  top: 100px;
-  width: 230px;
-`;
+// const StyledUl = styled(motion.ul)`
+//   color: black;
+//   margin: 0;
+//   padding: 0;
+//   padding: 25px;
+//   position: absolute;
+//   top: 100px;
+//   width: 230px;
+// `;
