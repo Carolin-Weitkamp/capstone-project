4660f5093d3515b555edff09c78c40dc7e89eb0c
Carolin Weitkamp <c.weitkamp@posteo.de>
2023-01-05 19:25:18 +0100

    imprint area

--- components/Legalbar/LegalBar.js
+++ components/Legalbar/LegalBar.js
@@ -24,6 +24,37 @@ const LegalBarStyle = styled.div`
   z-index: 1;
   border: ${({ theme }) => theme.border};
   box-shadow: pink;
+  width: 40px;
+  height: 100px;
+  bottom: 100px;
+  right: 0;
+  /* background: ${({ theme }) => theme.navbar}; */
+  cursor: pointer;
+  & :hover {
+    color: ${({ theme }) => theme.border};
+  }
+  > p {
+    writing-mode: vertical-rl;
+    transform:rotate(180deg);
+    position: fixed;
+    padding-left: 10px;
+    font-size: 0.9rem;
+    transition: 0, 2s;
+    right: 0;
+    margin-right: 0px;
+    & :hover {
+      color: hotpink;
+    }
+  }
+
+  @media screen and (min-width: 700px) {
+    position: fixed;
+  display: flex;
+  justify-content: start;
+  align-items: center;
+  z-index: 1;
+  border: ${({ theme }) => theme.border};
+  box-shadow: pink;
   width: 100%;
   height: 40px;
   bottom: 0px;
@@ -34,8 +65,10 @@ const LegalBarStyle = styled.div`
     color: ${({ theme }) => theme.border};
   }
   > p {
-    /* font-weight: light; */
     position: fixed;
+    direction: rtl;
+    writing-mode: horizontal-tb;
+    transform:rotate(360deg);
     padding-left: 30px;
     font-size: 0.9rem;
     transition: 0, 2s;
